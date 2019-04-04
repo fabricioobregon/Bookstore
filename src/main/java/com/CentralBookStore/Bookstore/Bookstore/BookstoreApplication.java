6ec8760ded75859a3f676a4dd12bfaa877bec84a
Fabricio Pires Obregon <fabricio@serti.com.br>
2019-04-04 05:41:59 -0600

    Yuhul! The app survived cohabiting with Spring Boot@

--- src/main/java/com/CentralBookStore/Bookstore/Bookstore/BookstoreApplication.java
+++ src/main/java/com/CentralBookStore/Bookstore/Bookstore/BookstoreApplication.java
@@ -1,16 +1,15 @@
 package com.CentralBookStore.Bookstore.Bookstore;
 
 import com.CentralBookStore.Bookstore.Bookstore.Service.StoreManager;
-import com.CentralBookStore.Bookstore.Bookstore.Utilities.ReadKeyboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(BookstoreApplication.class, args);
+		SpringApplication.run(BookstoreApplication.class, args);
 
 		StoreManager storeManager;
 		while (true){
